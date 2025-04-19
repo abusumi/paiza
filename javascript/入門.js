16aef3fe45446f4b03ebe37b6148a60ab80eb998
abusumi <hpbd12201204@gmail.com>
2025-04-19 19:00:00 +0900

    fix:file

--- javascript/入門.js
+++ javascript/入門.js
@@ -26,3 +26,14 @@ if (age < 20) {
     console.log("飲酒可能");
 }
 
+// if文による条件分岐　比較演算子
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+var place = parseInt(Math.random() * 12) + 1;
+process.stdout.write (place + "位");
+// ここにif文を追加する
+if (place <= 6) {
+    console.log("入賞");
+} else {
+    console.log("入賞圏外");
+}
