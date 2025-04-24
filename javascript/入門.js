a9f33e7e69ed5b0f142d6b68b76d1dc031ccf206
abusumi <hpbd12201204@gmail.com>
2025-04-24 19:00:00 +0900

    fix:file

--- javascript/入門.js
+++ javascript/入門.js
@@ -214,3 +214,15 @@ for (var seireki = 1926; seireki <=1988; seireki++){
 }
 
 // 01:配列とは何かを学ぼう
+
+// ＃02:配列を作ろう
+// 配列の中身を出力してみよう
+
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+
+var party = ["戦士", "侍", "僧侶", "魔法使い"];
+
+// ここで配列を出力する
+console.log (party);
+
