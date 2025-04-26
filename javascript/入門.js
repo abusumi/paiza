045b25fccd3deeb053ab42eb328435122da0404c
abusumi <hpbd12201204@gmail.com>
2025-04-26 19:00:00 +0900

    fix:file

--- javascript/入門.js
+++ javascript/入門.js
@@ -276,3 +276,14 @@ weapon.push("石斧");
 
 console.log(weapon);
 
+// 配列の要素を上書きしてみよう
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+var weapon = ["木の棒", "鉄の棒", "鉄の剣", "鋼の剣"];
+
+// ここで配列の要素を上書きする
+weapon[3] = "石斧";
+
+console.log(weapon);
