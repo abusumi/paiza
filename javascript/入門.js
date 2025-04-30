aad5da0982f876ebb7406b14d8a01dcb87c3f734
abusumi <hpbd12201204@gmail.com>
2025-04-30 19:00:00 +0900

    fix:file

--- javascript/入門.js
+++ javascript/入門.js
@@ -314,3 +314,15 @@ var cities = ["Tokyo", "Kanagawa", "Chiba", "Shizuoka", "Saitama"];
 cities.forEach(value => {
     console.log(value);
 })
+
+
+// 文字列をカンマで分割してみよう
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+var team_str = "勇者,戦士,忍者,魔法使い";
+
+// splitで分割し配列に格納、標準出力に出力する
+var team = team_str.split(",");
+console.log(team);
