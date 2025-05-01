fe54b3de508ff66e46dd623f4c003010e3517c8e
abusumi <hpbd12201204@gmail.com>
2025-05-01 19:00:00 +0900

    fix:file

--- javascript/入門.js
+++ javascript/入門.js
@@ -352,3 +352,23 @@ reader.on('close', () => {
 });
 
 // ＃08:複数行のデータを読み取ろう
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+var lines = [];
+var reader = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+reader.on('line', (line) => {
+    // 配列に標準入力を追加する
+    lines.push(line);
+});
+
+reader.on('close', () => {
+    // 配列の要素を繰り返し出力する
+    for(var value of lines){
+        console.log(value + "が現れた");
+    }
+});
